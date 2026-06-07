ac369ad4e2e00c51ea42f93caba8599d1be10d30
Abdallahadel2004 <abdo96650243@gmail.com>
2026-06-07 12:49:10 +0300

    feat: establish One-to-Many association (Creator 1:M Post)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: 'creator_id', class_name: 'Post', dependent: :destroy
+
   validates :name, :dob, :email, :phone_number, :address, presence: true
   validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/ }
 end
